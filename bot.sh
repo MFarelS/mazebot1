593abb71ba0803ab253d10c9e2b28268788b270e
visusvloid <91681617+visusvloid@users.noreply.github.com>
2021-10-07 16:08:03 +0700

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,12 @@
+echo -e "\e[93m               ________        ________
+             |                \   /               |
+             |                  ||                 |
+              \                                  /
+                \                              /
+                  \                          /
+                   |                         |
+     -----!    M   A   Z   E   B   O   T    !-----
+
+\e[0m";
+echo -e "Username :";
+Link  Produk :
