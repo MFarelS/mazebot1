35650c83efa4f28c8497a991ea0f3ed2c7115482
visusvloid <91681617+visusvloid@users.noreply.github.com>
2021-10-08 10:33:07 +0700

    Add files via upload

---
+++ m.sh
@@ -0,0 +1,15 @@
+echo -e "\e[31m\# M a z e B o t \#\e[0m";
+read -p 'Username : ' userinput1;
+#
+if [ "${userinput1:-}" = "https://shopee.co.id" ]
+then
+	echo -e "\e[32mSukses Bro!\e[0m";
+fi
+readlink https://shopee.co.id
+pipe_progress https://shopee.co.id
+scriptreplay menit : 
+uptime timer :
+timeout jam : 
+touch https://shopee.co.id
+date proses....
+uname username =
